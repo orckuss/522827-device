beb10ac981e578c2f2807e997c9fe143437099a2
orckuss <33227031+orckuss@users.noreply.github.com>
2017-12-10 02:04:20 +0300

    оживление формы обратной связи

--- js/scripts.js
+++ js/scripts.js
@@ -5,21 +5,20 @@ var popup_close = popup.querySelector(".modal-close");
 var form = popup.querySelector(".feedback");
 var form_name = form.querySelector("#name");
 var form_email = form.querySelector("#email")
-
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
   form_name.focus();
 });
-
 form.addEventListener("submit", function (evt) {
   if (!form_name.value || !form_email.value) {
     evt.preventDefault();
-    console.log("bla bla bla");
   }
 });
-
 popup_close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
 });
+window.addEventListener("keydown"
+
+)
